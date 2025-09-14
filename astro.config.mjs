1704631bf830cabe8f2a435f813cb4dc51becdb2
Martin Currie <martin.currie@aqueum.com>
2025-09-14 01:44:13 +0100

    remove social

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
-// @ts-check
+
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{ label: 'Docs', autogenerate: { directory: 'docs' } },
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: 'Sustainable Survival',
+      sidebar: [
+        { label: 'Docs', autogenerate: { directory: 'docs' } },
+      ],
+    }),
+  ],
 });
